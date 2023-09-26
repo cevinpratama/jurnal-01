57669b8a7434b842e19df69dd5fc8932e2082ea6
cevinpratama <cevinpubg8@gmail.com>
2023-09-26 10:16:15 +0700

    first commit

---
+++ jurnal01.java
@@ -0,0 +1,8 @@
+public class jurnal01{
+    public static void main(String[] args) {
+     System.out.println("NAME : CEVIN PRATAMA SUNDORA");
+     System.out.println("NIM : 607062300141");
+     System.out.println("KELAS : D3IF-47-03 ");
+     
+    }
+}
